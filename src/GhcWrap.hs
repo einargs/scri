1c77b2f3086989ce3fa1962204bd4fc7a69b1cd7
einargs <einargs@gmail.com>
2018-09-09 03:41:27 -0500

    Checkpoint

--- src/GhcWrap.hs
+++ src/GhcWrap.hs
@@ -4,7 +4,50 @@ import GHC.Paths
 import Control.Monad (forM_)
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Data.Dynamic
+import System.FilePath
 
+
+-- | Import a file and shepard it through the compilation process.
+-- This is to allow easy access to the module name without having
+-- to mess around with the depedency graph in possibly breakable
+-- ways.
+--
+-- There is a very good chance that this is a fairly bad way of doing
+-- this, but it will have to suffice for the time being.
+--
+-- Note that @fileName@ is only read in for the purposes of being passed
+-- to the parser interface. 
+-- 
+-- NOTE: It may not be possible to shepard compilation without invoking
+-- the load target stuff and potentially duplicating effort. It seems as
+-- though this really comes down to whether I can generate a 'ModSummary'
+-- on my own or not, seeing as 'parseModule', 'typecheckModule', and
+-- 'loadModule' seem to allow me to do so on my own.
+--
+-- To find an answer to this question I should continue reading through
+-- that "Dive into GHC" series of blog posts.
+--
+-- I should also consider whether or not it might be better to just
+-- use the load target stuff for now instead of being a perfectionist.
+-- Now that I can properly access the module name, I'm in a much better
+-- position (though honestly, there is a small chance none of this was
+-- necessary. It helped me learn though, so it's not terrible).
+compileSrc :: String -> String -> Ghc ()
+compileSrc fileSrc fileName = do
+  dflags <- getSessionDynFlags
+  let (warningMessages, Right locWrapper) = parser fileSrc dflags fileName 
+      hsModule = unLoc locWrapper
+      (Just locModName) = hsmodName hsModule
+      modName = unLoc locModName
+  return ()
+
+
+importFile :: String -> Ghc ()
+importFile path = do
+  fileSrc <- liftIO $ readFile path
+  let fileName = takeFileName path
+  result <- compileSrc fileSrc fileName
+  return ()
 
 
 initSession :: IO HscEnv
@@ -35,7 +78,7 @@ initSession = runGhc (Just libdir) $ do
 
   -- The module name for the example module 
   let exampleModSum:_ = modSums
-      exampleModName = ms_mod_name modSum
+      exampleModName = ms_mod_name exampleModSum
   
   -- You can print the module name with
   -- @liftIO $ print $ moduleNameString modName@
@@ -44,7 +87,7 @@ initSession = runGhc (Just libdir) $ do
   -- Append the module name of Example.hs to the imports into the interactive
   -- context.
   ctx <- getContext
-  setContext ((IIModule modName):ctx)
+  setContext ((IIModule exampleModName):ctx)
 
   -- Return the environment
   env <- getSession
