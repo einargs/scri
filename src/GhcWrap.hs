68235c9ae27a9756fa699bccd1bf51f4a54e3708
einargs <einargs@gmail.com>
2018-09-09 13:59:47 -0500

    Improved ability to import modules.
    Also, I think that I need to change the way modules will be imported in Scri.
    Instead of using relative file paths, I think that it would be better if I just
    used Haskell modules.

--- src/GhcWrap.hs
+++ src/GhcWrap.hs
@@ -7,47 +7,40 @@ import Data.Dynamic
 import System.FilePath
 
 
--- | Import a file and shepard it through the compilation process.
--- This is to allow easy access to the module name without having
--- to mess around with the depedency graph in possibly breakable
--- ways.
---
--- There is a very good chance that this is a fairly bad way of doing
--- this, but it will have to suffice for the time being.
---
--- Note that @fileName@ is only read in for the purposes of being passed
--- to the parser interface. 
--- 
--- NOTE: It may not be possible to shepard compilation without invoking
--- the load target stuff and potentially duplicating effort. It seems as
--- though this really comes down to whether I can generate a 'ModSummary'
--- on my own or not, seeing as 'parseModule', 'typecheckModule', and
--- 'loadModule' seem to allow me to do so on my own.
---
--- To find an answer to this question I should continue reading through
--- that "Dive into GHC" series of blog posts.
---
--- I should also consider whether or not it might be better to just
--- use the load target stuff for now instead of being a perfectionist.
--- Now that I can properly access the module name, I'm in a much better
--- position (though honestly, there is a small chance none of this was
--- necessary. It helped me learn though, so it's not terrible).
-compileSrc :: String -> String -> Ghc ()
-compileSrc fileSrc fileName = do
+-- | Get a module name from the source.
+getModNameFromSrc :: String -> String -> Ghc ModuleName
+getModNameFromSrc fileSrc fileName = do
   dflags <- getSessionDynFlags
   let (warningMessages, Right locWrapper) = parser fileSrc dflags fileName 
       hsModule = unLoc locWrapper
       (Just locModName) = hsmodName hsModule
       modName = unLoc locModName
-  return ()
+  return modName
 
 
-importFile :: String -> Ghc ()
-importFile path = do
-  fileSrc <- liftIO $ readFile path
-  let fileName = takeFileName path
-  result <- compileSrc fileSrc fileName
-  return ()
+-- | Add an interactive import to the context.
+addToContext :: InteractiveImport -> Ghc ()
+addToContext x = do
+  ctx <- getContext
+  setContext (x:ctx)
+
+-- | Load and import a module specified by an import statement.
+runImportStmt :: String -> Ghc ()
+runImportStmt stmt = do
+  -- Parse the import statement
+  importDecl <- parseImportDecl stmt
+
+  -- Get the module name and create the target
+  let modName = unLoc . ideclName $ importDecl
+      modTargetId = TargetModule modName
+      modTarget = Target modTargetId True Nothing
+
+  -- Add the target and load it
+  addTarget modTarget
+  load $ LoadUpTo modName
+
+  -- Add the import to the interactive context
+  addToContext $ IIDecl importDecl
 
 
 initSession :: IO HscEnv
@@ -61,11 +54,9 @@ initSession = runGhc (Just libdir) $ do
   -- in the context before this.
   setContext [IIDecl $ simpleImportDecl (mkModuleName "Prelude")]
 
-  -- Load Example.hs from the root of the project
-  target <- guessTarget "Example.hs" Nothing
-  addTarget target
-  load LoadAllTargets
-
+  -- Run the import statement
+  runImportStmt "import Example (locCommand)"
+  
   -- Get the module graph, which now contains MainTest, the module
   -- that Example.hs contains
   modGraph <- getModuleGraph
@@ -75,19 +66,6 @@ initSession = runGhc (Just libdir) $ do
   -- You can print all of the file names of the module summaries
   -- (Or whatever other property you want to print)
   liftIO $ mapM_ print (map ms_hspp_file modSums)
-
-  -- The module name for the example module 
-  let exampleModSum:_ = modSums
-      exampleModName = ms_mod_name exampleModSum
-  
-  -- You can print the module name with
-  -- @liftIO $ print $ moduleNameString modName@
-  -- The output should be @"MainTest"@ (quotes included because of 'print')
-  
-  -- Append the module name of Example.hs to the imports into the interactive
-  -- context.
-  ctx <- getContext
-  setContext ((IIModule exampleModName):ctx)
 
   -- Return the environment
   env <- getSession
